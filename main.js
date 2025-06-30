c5b69e65d3e6d9921e171e8dbdd06d4754013b93
0024sobit <0024sobitvft@gmail.com>
2025-06-30 16:58:11 +0530

    .

--- main.js
+++ main.js
@@ -263,7 +263,7 @@ function initSlideshow() {
     clearInterval(timer);
     currentIndex = index;
     showSlide(currentIndex);
-    timer = setInterval(nextSlide, 7030);
+    timer = setInterval(nextSlide, 7000);
   }
 
   function nextSlide() {
@@ -275,7 +275,7 @@ function initSlideshow() {
   });
 
   showSlide(currentIndex);
-  timer = setInterval(nextSlide, 7030);
+  timer = setInterval(nextSlide, 7000);
 }
 
 
